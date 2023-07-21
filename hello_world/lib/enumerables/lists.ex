d955f98f8ce4df8ae7db9afef531061637995799
Jeff Mosawy <me@jmosawy.com>
2023-07-21 23:19:34 +0200

    feat: add enumerable module

---
+++ hello_world/lib/enumerables/lists.ex
@@ -0,0 +1,20 @@
+defmodule HelloWorld.Enumerables.Lists do
+  @moduledoc """
+  Function Summary:
+
+  1. sum_simple
+  2. sum_tail_rec
+  """
+
+  @doc """
+  Returns the sum of numbers in a list
+  """
+  def sum_simple([]), do: 0
+
+  def sum_simple([h | t]), do: h + sum_simple(t)
+
+
+  def sum_tail_rec(nums, acc \\ 0)
+  def sum_tail_rec([], acc), do: acc
+  def sum_tail_rec([h | t], acc), do: sum_tail_rec(t, acc + h)
+end
